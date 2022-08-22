5b703f73f3f8e2446c56801f6e3042259dacedf9
Kevin <kevin@kevinisageek.org>
2022-08-22 09:09:23 +0100

    added empty desktop file, added ssd support to nomadic

--- hosts/nomadic/configuration.nix
+++ hosts/nomadic/configuration.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  # stuff
+  options = {
+    nomadic.ssd.enable = lib.mkEnableOption "Enables TRIM support for SSDs";
+  };
+
+  config = lib.mkIf config.nomadic.ssd.enable {
+    boot.kernel.sysctl = { "vm.swappiness" = lib.mkDefault 1; };
+    services.fstrim.enable = lib.mkDefault true;
+  };
 }
